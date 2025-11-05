46b5ca72da4ad2bbaf2a5cc88384b424e0aba008
Matheus G. P. de Carlucci <mgiussepe26@gmail.com>
2025-11-05 18:12:50 -0300

    Fix: rotas de vale-alimentação e boletim

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -44,12 +44,12 @@ const data = {
     },
     {
       title: "Vale Alimentação",
-      url: "#",
+      url: "/vale-alimentacao",
       icon: Utensils,
     },
     {
       title: "Boletim",
-      url: "#",
+      url: "/boletim",
       icon: File,
     },
   ],
